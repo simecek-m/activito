cb192d6bbd13dfb55f3e14ab7f5509ae772c53e0
Martin Simecek <simecek.m@seznam.cz>
2019-01-30 20:09:27 +0100

    add SplashScreen with Lottie animation

---
+++ app/src/main/java/com/example/activito/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.activito.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.activito.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
