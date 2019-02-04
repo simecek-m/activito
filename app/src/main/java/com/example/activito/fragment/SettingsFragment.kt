e928998d14da164029b3bc32bf326a16b0f13061
Martin Simecek <simecek.m@seznam.cz>
2019-02-04 14:13:34 +0100

    refaktoring - odstranění zbytečných vygenerovaných argumentů ve fragmentech

--- app/src/main/java/com/example/activito/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/activito/fragment/SettingsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.activito.fragment
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,23 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.activito.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class SettingsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
