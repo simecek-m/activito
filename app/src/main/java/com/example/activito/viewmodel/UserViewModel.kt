bbd148eb2d5f4b32dcb218cb68b72a487d9cf874
Martin Simecek <simecek.m@seznam.cz>
2019-02-06 11:44:09 +0100

    show user weight and height on Profile screen

--- app/src/main/java/com/example/activito/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/activito/viewmodel/UserViewModel.kt
@@ -3,15 +3,27 @@ package com.example.activito.viewmodel
 import android.app.Application
 import android.content.Intent
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.activito.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.api.Scope
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.data.Field
+import com.google.android.gms.fitness.request.DataReadRequest
+import com.google.android.gms.fitness.result.DataReadResponse
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class UserViewModel(application: Application): AndroidViewModel(application) {
 
-    var currentUser:GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(getApplication())
+    var currentUser: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(getApplication())
+
+    var height: MutableLiveData<Float> = MutableLiveData()
+    var weight:MutableLiveData<Float> = MutableLiveData()
+
 
     private val googleSignInOpetions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
         .requestEmail()
@@ -28,6 +40,30 @@ class UserViewModel(application: Application): AndroidViewModel(application) {
     fun signOut(){
         googleSignInClient.signOut()
         currentUser = null
+    }
+
+    fun getBodyInfoRequest():DataReadRequest{
+        val calendar = Calendar.getInstance()
+        calendar.time = Date()
+
+        return DataReadRequest.Builder()
+            .read(DataType.TYPE_HEIGHT)
+            .read(DataType.TYPE_WEIGHT)
+            .setTimeRange(1, calendar.timeInMillis, TimeUnit.MILLISECONDS)
+            .setLimit(1)
+            .build()
+    }
+
+    fun setBodyInfo(response: DataReadResponse) {
+        weight.value = response.getDataSet(DataType.TYPE_WEIGHT)
+            .dataPoints.lastOrNull()
+            ?.getValue(Field.FIELD_WEIGHT)
+            ?.asFloat()
+
+        height.value = response.getDataSet(DataType.TYPE_HEIGHT)
+            .dataPoints.lastOrNull()
+            ?.getValue(Field.FIELD_HEIGHT)
+            ?.asFloat()
     }
 
 }
