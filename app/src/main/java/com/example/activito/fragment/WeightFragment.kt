b77f2970be88ad6dfa9993d952d18529a8a3e5da
Martin Simecek <simecek.m@seznam.cz>
2019-02-17 23:19:32 +0100

    redesign grafu váhy

--- app/src/main/java/com/example/activito/fragment/WeightFragment.kt
+++ app/src/main/java/com/example/activito/fragment/WeightFragment.kt
@@ -1,7 +1,6 @@
 package com.example.activito.fragment
 
 import android.content.ContentValues.TAG
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,10 +14,13 @@ import com.example.activito.animation.Animation
 import com.example.activito.dialog.WeightPickerDialogFragment
 import com.example.activito.viewmodel.UserViewModel
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.google.android.gms.fitness.Fitness
 import kotlinx.android.synthetic.main.fragment_weight.*
+import java.text.SimpleDateFormat
 
 class WeightFragment : Fragment() {
 
@@ -59,35 +61,47 @@ class WeightFragment : Fragment() {
             .addOnSuccessListener { response ->
                 if (isAdded){
                     val entries = userViewModel.getWeightProgressChartPoints(response)
-                    val dataSet = LineDataSet(entries, "leden")
-                    val primaryColor = ContextCompat.getColor(activity!!, R.color.colorPrimary)
-                    dataSet.apply {
-                        setCircleColor(Color.WHITE)
-                        circleHoleColor = primaryColor
-                        color = primaryColor
-                        setDrawFilled(true)
-                        fillColor = primaryColor
-                        fillAlpha = 255
-                        lineWidth = 3f
-                        circleRadius = 10f
-                        circleHoleRadius = 5f
-                        valueTextSize = 20f
-                        valueTextColor = primaryColor
-                        mode = LineDataSet.Mode.HORIZONTAL_BEZIER
-                    }
-
-                    (chart as LineChart).apply {
-                        setTouchEnabled(false)
-                        data = LineData(dataSet)
-                        description = null
-                        xAxis.isEnabled = false
-                        axisRight.isEnabled = false
-                        axisLeft.isEnabled = false
-                        invalidate()
-                        setNoDataText(getString(R.string.loading_weight_data))
-                        setNoDataTextColor(ContextCompat.getColor(context, R.color.colorPrimary))
-                    }
+                    val dataSet = initDataSet(entries)
+                    initLineChart(chart)
+                    chart.data = LineData(dataSet)
                 }
             }.addOnFailureListener{e -> Log.e(TAG, "weight progress load failure: ", e)}
+    }
+
+    private fun initDataSet(entries: List<Entry>):LineDataSet{
+        val primaryColor = ContextCompat.getColor(activity!!, R.color.colorPrimary)
+        val dataSet = LineDataSet(entries, "")
+        return dataSet.apply {
+            setCircleColor(primaryColor)
+            circleHoleColor = primaryColor
+            color = primaryColor
+            lineWidth = 3f
+            circleRadius = 5f
+            circleHoleRadius = 3f
+            setDrawHighlightIndicators(false)
+            setDrawValues(false)
+            valueTextColor = primaryColor
+            mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+        }
+    }
+
+    private fun initLineChart(chart: LineChart){
+        chart.apply {
+            setTouchEnabled(true)
+            invalidate()
+            setDrawMarkers(true)
+            xAxis.setDrawAxisLine(true)
+            xAxis.setDrawGridLines(false)
+            legend.isEnabled = false
+            description = null
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            xAxis.setValueFormatter { value, _ ->  SimpleDateFormat("dd MMM").format(value)}
+            axisLeft.setDrawGridLines(false)
+            axisRight.setDrawAxisLine(false)
+            axisRight.setDrawLabels(false)
+            axisRight.setDrawGridLines(false)
+            setNoDataText(getString(R.string.no_weight_data_available))
+            setNoDataTextColor(ContextCompat.getColor(context, R.color.colorPrimary))
+        }
     }
 }
